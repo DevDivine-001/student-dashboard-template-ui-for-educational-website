9cc0d7b3ade564247d29d8d1def94b86a3f6d606
Oscdivin <divineuwandu84@Gmail.com>
2024-01-19 23:25:58 +0100

    veiwslife

--- src/Pages/Home/View.tsx
+++ src/Pages/Home/View.tsx
@@ -1,10 +1,10 @@
 const View = () => {
   return (
     <section className="">
-      {/* <div className="flex justify-between p-3 w-[100%] bg-[] flex-wrap max-sm:flex-row gap-3">
+      <div className="flex justify-between p-3 w-[100%] bg-[] flex-wrap max-sm:flex-row gap-3">
         <div className=" max-md:w-[100%] max-lg:w-full h-[300px] w-[75%] bg-[#000]"></div>
         <div className="max-md:w-full max-lg:w-full h-[300px] w-[22%] bg-[blue]"></div>
-      </div> */}
+      </div>
     </section>
   );
 };
